2ca6b62d8db08968fe32ffdaa79f90f8fe32f57f
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-08-07 02:03:02 -0500

    padiind

--- ask.js
+++ ask.js
@@ -7,7 +7,7 @@ let https = require('https');
 const app = express()
 
 
-app.set( 'port', ( process.env.PORT || 8000 ));
+app.set( 'port', ( process.env.PORT || 8080 ));
 
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(__dirname + '/ask_assets/'));
